70549afcc598c0e64d8dde85bfb68f097e94860f
Noah Manno <noahmanno19@gmail.com>
2024-06-04 08:29:52 -0400

    added index model

---
+++ models/index.js
@@ -0,0 +1,16 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, )
+
+module.exports = { User, Post, Comment };
