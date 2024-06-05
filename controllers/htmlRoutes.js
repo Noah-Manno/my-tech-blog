9b9ffc3eaa3fe155f442c86d774a93c14a5abc7b
Noah Manno <noahmanno19@gmail.com>
2024-06-05 07:34:34 -0400

    add

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User, Post } = require('../models');
+const withAuth = require('../utils/withAuth')
 
 router.get('/', async (req, res) => {
     try {
@@ -7,6 +8,30 @@ router.get('/', async (req, res) => {
         const posts = postData.map((post) => post.get({ plain: true}));
         console.log(posts)
         res.render('homepage', { posts });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/login', (req, res) => {
+    try {
+        res.render('login');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/signup', (req, res) => {
+    try {
+        res.render('signup');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/dashboard', withAuth, (req, res) => {
+    try {
+        res.render('dashboard');
     } catch (err) {
         res.status(500).json(err);
     }
